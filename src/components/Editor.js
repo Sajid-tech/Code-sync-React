cf999e016d2a709dfdbdd4896a898914795a6e69
Sajid-tech <80850448+Sajid-tech@users.noreply.github.com>
2023-07-05 00:37:39 +0530

    editor done

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -11,22 +11,23 @@ import 'codemirror/addon/edit/closebrackets'
 const Editor = () => {
 
     useEffect(() => {
-        async function init() {
-            Codemirror.fromTextArea(document.getElementById('realtimeEditor'), {
-                // to enable thi mode we have to import this -: import  'codemirror/mode/javascript/javascript'
-                mode: { name: 'javascript', json: true },
-                //to enable theme mode we have to import this : import 'codemirror/theme/darcula.css'
-                theme: 'darcula',
-                //to enbale thi swe have to import this:import 'codemirror/addon/edit/closetag'
-                autoCloseTags: true,
-                //to enable this we have ti import this:import 'codemirror/addon/edit/closebrackets'
-                autoCloseBrackets: true,
-                lineNumbers: true
 
-            });
-        }
-        init()
-    }, [])
+        Codemirror.fromTextArea(document.getElementById('realtimeEditor'), {
+            // to enable thi mode we have to import this -: import  'codemirror/mode/javascript/javascript'
+            mode: { name: 'javascript', json: true },
+            //to enable theme mode we have to import this : import 'codemirror/theme/darcula.css'
+            theme: 'darcula',
+            //to enbale thi swe have to import this:import 'codemirror/addon/edit/closetag'
+            autoCloseTags: true,
+            //to enable this we have ti import this:import 'codemirror/addon/edit/closebrackets'
+            autoCloseBrackets: true,
+            lineNumbers: true
+
+
+
+
+        })
+    })
 
 
 
