fb191d3c480dffaa40dbcc9e101a67c3930498db
Sajid-tech <80850448+Sajid-tech@users.noreply.github.com>
2023-07-05 00:22:24 +0530

    editor-half done

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,22 +1,37 @@
 import React, { useEffect } from 'react'
 import Codemirror from 'codemirror'
+import 'codemirror/lib/codemirror.css'
 import 'codemirror/mode/javascript/javascript'
 import 'codemirror/theme/darcula.css'
+import 'codemirror/addon/edit/closetag'
+import 'codemirror/addon/edit/closebrackets'
 
 
 
 const Editor = () => {
+
     useEffect(() => {
         async function init() {
             Codemirror.fromTextArea(document.getElementById('realtimeEditor'), {
                 // to enable thi mode we have to import this -: import  'codemirror/mode/javascript/javascript'
                 mode: { name: 'javascript', json: true },
-                //theme
-                theme: 'darcula'
-            })
+                //to enable theme mode we have to import this : import 'codemirror/theme/darcula.css'
+                theme: 'darcula',
+                //to enbale thi swe have to import this:import 'codemirror/addon/edit/closetag'
+                autoCloseTags: true,
+                //to enable this we have ti import this:import 'codemirror/addon/edit/closebrackets'
+                autoCloseBrackets: true,
+                lineNumbers: true
+
+            });
         }
         init()
     }, [])
+
+
+
+
+
     return (
         <textarea id='realtimeEditor'></textarea>
     )
