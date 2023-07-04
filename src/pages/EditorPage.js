5b42710fcf90cacf7f34d457de18120ecc4de11f
Sajid-tech <80850448+Sajid-tech@users.noreply.github.com>
2023-07-04 23:01:43 +0530

    EditorPage done succesfully

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Client from '../components/Client'
+import Editor from '../components/Editor'
+
 
 const EditorPage = () => {
+    const [clients, setClients] = useState([
+        { socketId: 1, username: 'sajid k' },
+        { socketId: 2, username: 'majid k' },
+        { socketId: 3, username: 'wajid k' },
+
+    ])
     return (
-        <div>Editor Page</div>
+        <div className='mainWrap'>
+            <div className="aside">
+                <div className="asideInner">
+                    <div className="logo">
+                        <img className='logoImage' src="/code-sync.png" alt="code-sync logo" />
+                    </div>
+                    <h3>Connected</h3>
+                    <div className="clientsList">{
+                        clients.map((client) => (<Client key={client.socketId} username={client.username} />))
+                    }</div>
+
+                </div>
+                <button className='btn copyBtn' >Copy ROOM ID</button>
+                <button className='btn leaveBtn'>Leave</button>
+            </div>
+            <div className="editorWrap">
+                <Editor />
+            </div>
+        </div>
     )
 }
 
